3f62f7a59bbd80eb3c305156a866feaaca8bacb6
Lutong <lo2272@nyu.edu>
2026-05-01 12:36:01 +0800

    1

---
+++ Project2/script3.js
@@ -0,0 +1,4 @@
+let neverFade = document.createElement("p");
+neverFade.innerText = "But your eternal summer shall not fade, Nor lose the beauty that you possess."
+let live = document.createElement("p");
+live.innerText = "As long as men can breathe or eyes can see, So long lives this poem, and this gives life to you."
